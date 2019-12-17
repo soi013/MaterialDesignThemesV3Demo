0dad50a4f558844f8d76b06eedbffff23d484267
soi013 <soi2flatline@gmail.com>
2019-12-17 23:49:29 +0900

    プロジェクト ファイルを追加します。

---
+++ MaterialDesignThemesV3Demo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemesV3Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
